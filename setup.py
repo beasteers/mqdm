bfeadd1da0c7ce6679297de41eb5fed9ac78de70
Bea Steers <bea.steers@gmail.com>
2024-10-02 11:33:03 -0400

    bump 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mqdm',
-    version='0.1.0',
+    version='0.1.1',
     description='',
     long_description=open('README.md').read().strip(),
     long_description_content_type='text/markdown',
