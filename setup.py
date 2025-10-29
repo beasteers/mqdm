b2b95b29cd19a2948fdd9ca0ca65e9fd8feac6ec
Bea Steers <bea.steers@gmail.com>
2025-10-29 13:48:52 -0400

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name='mqdm',
-    version='1.1.0',
+    version='1.2.0',
     description='cross-process progress bars',
     long_description=open('README.md').read().strip(),
     long_description_content_type='text/markdown',
